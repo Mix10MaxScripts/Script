466c42cd2d3ec90618fbd675eadd9c584a8f4731
Maxim <84537613+Mix10MaxScripts@users.noreply.github.com>
2021-11-30 15:20:56 +0100

    Add files via upload

---
+++ script.lua
@@ -0,0 +1,4 @@
+game.Players.LocalPlayer.leaderstats.Rebirths.Value = 99999999999999
+game.Players.LocalPlayer.leaderstats.Strength.Value = 99999999999999
+game.Players.LocalPlayer.leaderstats.Kills.Value = 99999999999999
+game.Players.LocalPlayer.leaderstats.Brawls.Value = 99999999999999
